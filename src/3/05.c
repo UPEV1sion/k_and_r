591c00d56e8351577c9a80a11a8140dc4dd85745
UPEV1sion <emanuel.schaeffer@proton.me>
2026-01-10 16:46:40 +0100

    formating

--- src/3/05.c
+++ src/3/05.c
@@ -5,43 +5,43 @@
 
 void reverse(char *s)
 {
-	const size_t len = strlen(s);
+    const size_t len = strlen(s);
 
-	for(size_t i = 0; i < len/2; ++i)
-	{
-		const char temp = s[i];
-		s[i] = s[len-1-i];
-		s[len-1-i] = temp;
-	}
+    for(size_t i = 0; i < len/2; ++i)
+    {
+        const char temp = s[i];
+        s[i] = s[len-1-i];
+        s[len-1-i] = temp;
+    }
 }
 
 void itob(int num, char *s, const int base)
 {
-	static const char conv[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+    static const char conv[] = "0123456789abcdefghijklmnopqrstuvwxyz";
 
-	int sign = num;
-	int i = 0;	
+    int sign = num;
+    int i = 0;    
 
-	do
-	{
-		s[i++] = conv[abs(num % base)];
-	} while(num /= base);
+    do
+    {
+        s[i++] = conv[abs(num % base)];
+    } while(num /= base);
 
-	if(sign < 0) s[i++] = '-';
+    if(sign < 0) s[i++] = '-';
 
-	s[i] = 0;
+    s[i] = 0;
 
-	reverse(s);
+    reverse(s);
 }
 
 int main(void)
 {
-	char buffer[1024];
-	itob(69, buffer, 16);
+    char buffer[1024];
+    itob(69, buffer, 16);
 
-	puts(buffer);
+    puts(buffer);
 
-	return 0;
+    return 0;
 }
 
-	
+    

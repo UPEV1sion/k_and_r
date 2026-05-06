61b280ff1c6c45bd734a991bdd5c18b0b4f226bb
UPEV1sion <e.schaeffer82@gmail.com>
2026-05-06 21:30:18 +0200

    8-7

---
+++ src/8/07.c
@@ -0,0 +1,144 @@
+#define _DEFAULT_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#define MAXBYTES (unsigned int) 1000000
+
+typedef long Align;
+typedef union header Header;
+union header {
+    struct {
+        union header *ptr;
+        unsigned size;
+    } s;
+    Align x;
+};
+
+#define NALLOC 1024
+static Header base;
+static Header *freep = NULL;
+
+static Header* morecore(unsigned nu);
+
+void xfree(void *ap)
+{
+    if(ap == NULL) return;
+
+    Header *bp = (Header *) ap - 1;
+
+    Header *p;
+    for(p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+    {
+        if(p >= p->s.ptr && (bp > p || bp < p->s.ptr))
+        {
+            break;
+        }
+    }
+
+    if(bp + bp->s.size == p->s.ptr)
+    {
+        bp->s.size += p->s.ptr->s.size;
+        bp->s.ptr = p->s.ptr->s.ptr;
+    }
+    else
+    {
+        bp->s.ptr = p->s.ptr;
+    }
+
+    if(p + p->s.size == bp)
+    {
+        p->s.ptr = bp->s.ptr;
+        p->s.size += bp->s.size;
+    }
+    else
+    {
+        p->s.ptr = bp;
+    }
+
+    freep = p;
+}
+
+void* xmalloc(unsigned nbytes)
+{
+    if(nbytes == 0 || nbytes > MAXBYTES) return NULL;
+
+    const unsigned nunits = (nbytes + sizeof(Header) - 1)/sizeof(Header) + 1;
+    Header *prevp;
+    if((prevp = freep) == NULL)
+    {
+        base.s.ptr = freep = prevp = &base;
+        base.s.size = 0;
+    }
+
+    for(Header *p = prevp->s.ptr; ; prevp = p, p = p->s.ptr)
+    {
+        if(p->s.size >= nunits)
+        {
+            if(p->s.size == nunits)
+            {
+                prevp->s.ptr = p->s.ptr;
+            }
+            else
+            {
+                p->s.size -= nunits;
+                p += p->s.size;
+                p->s.size = nunits;
+            }
+
+            freep = prevp;
+            return (void *) (p + 1);
+        }
+
+        if(p == freep)
+        {
+            if((p = morecore(nunits)) == NULL)
+            {
+                return NULL;
+            }
+        }
+    }
+
+    return NULL;
+}
+
+void* xcalloc(const unsigned n, const unsigned size)
+{
+    const unsigned nbytes = n * size;
+    char *mem = xmalloc(nbytes);
+
+    memset(mem, 0, nbytes);
+
+    return mem;
+}
+
+static Header* morecore(unsigned nu)
+{
+    if(nu < NALLOC) nu = NALLOC;
+
+    char *cp = sbrk(nu * sizeof(Header));
+    if(cp == (char *) - 1) return NULL;
+
+    Header *up = (Header *) cp;
+    up->s.size = nu;
+    xfree((void *) (up + 1));
+
+    return freep;
+}
+
+int main(void)
+{
+    char *mem1 = xmalloc(0);
+    assert(mem1 == NULL);
+    char *mem2 = xmalloc(~0u);
+    assert(mem2 == NULL);
+    char *mem3 = xmalloc(100);
+    assert(mem3 != NULL);
+
+    xfree(mem3);
+
+    return 0;
+}
